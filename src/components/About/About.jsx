5c4d8e6717f3252fe3abc13abb44cfe73ae6027a
RamaBzoor <130461845+RamaBzoor@users.noreply.github.com>
2024-04-01 13:07:46 +0300

    message

---
+++ src/components/About/About.jsx
@@ -0,0 +1,18 @@
+
+import React, { useState } from 'react';
+
+
+function About() {
+
+  let [count,setCount] = useState(0);
+  const changeCount = ()=>{
+    count = count + 1;
+  };
+  return (
+    <div>
+      <button onClick={changeCount}>count : {count}</button>
+    </div>
+  )
+}
+
+export default About
